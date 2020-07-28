55fafde2c019aa76ec0832b1017081534a21f147
David Shober <shobes1421@hotmail.com>
2020-07-27 18:29:23 -0600

    commit final working app

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Alex Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sannar Sandwich', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie K Burger', FALSE);
+
